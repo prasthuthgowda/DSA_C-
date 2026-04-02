1342cd5684902d7c1d8fd2e5743bc9bde5f4de1e
Prasthuth Gowda <prasthuth123@gmail.com>
2026-04-02 16:46:45 +0530

    binary_number_systems

---
+++ binarySystem.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+int decToBinary(int decNum){
+    int ans = 0, pow = 1;
+
+    while(decNum > 0){
+        int rem = decNum % 2;
+        decNum /=2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+    return ans;
+
+}
+int binTodecimal(int binNum){
+    int ans = 0, pow = 1;
+
+    while( binNum > 0){
+        int rem = binNum % 10;
+        ans += rem * pow;
+
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
+int main(){
+    int decNum = 50;
+    // cout << decToBinary(decNum);
+    cout << binTodecimal(1010) <<endl;
+    return 0;
+}
