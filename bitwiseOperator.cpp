7c0617d96dc0b11351ac15880cb6329506e80c88
Prasthuth Gowda <prasthuth123@gmail.com>
2026-04-04 12:59:23 +0530

    bitwise_Operator

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){  
+    int a = 4, b = 8;
+    // int ans =  a ^ b;
+    int ans = a << b;
+    // cout<<(10 >> 1);
+    cout << sizeof(int) <<endl;
+    cout << sizeof(long) <<endl;
+    cout << sizeof(long int) <<endl;
+    cout << sizeof(short int) <<endl;
+    cout << sizeof(long long) <<endl;
+    cout << sizeof(int) <<endl;
+
+
+
+    return 0;
+}
