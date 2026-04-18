a7477a30294bf50b406e2febe6129533a22248d6
Prasthuth Gowda <prasthuth123@gmail.com>
2026-04-18 20:36:17 +0530

    Array

--- arrays.cpp
+++ arrays.cpp
@@ -8,6 +8,8 @@ int smallest(int a[], int size){
         if(a[i] < small){
             small = a[i];
         }
+        // also instead of if we can use
+        // min(a[i],small);
     }
     return small;
 
@@ -21,12 +23,32 @@ int largest(int a[] , int size){
         if(a[i] > large){
             large = a[i];
         }
+        // max(a[i], large);
     }
     return large;
 }
+
+int linearSearch(int arr[] , int target, int size){
+    for(int i=0;i<size;i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void reverseArr(int arr[], int size){
+    int start = 0 , end = size-1;
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }    
+}
+
 int main(){
 
-    int marks[] = {99, 54, 78, 91, 98};
+    // int marks[] = {99, 54, 78, 91, 98};
     // int size = 5;
     // cout<<marks[0] << endl;
     // cout<<marks[1] << endl;
@@ -36,9 +58,19 @@ int main(){
     // for(int i=0; i<size; i++){
     //     cout<< marks[i] <<endl;
     // }
-    int size = 6;
-    int a [] = {5, 15, 22, 1, -15 , 24};
-    cout <<"Smallest : "<< smallest(a , size) <<endl;
-    cout <<"Largest : " << largest(a, size) <<endl;
+    // int size = 6;
+    // int a [] = {5, 15, 22, 1, -15 , 24};
+    // cout <<"Smallest : "<< smallest(a , size) <<endl;
+    // cout <<"Largest : " << largest(a, size) <<endl;
+    int arr[] = {4,2,7,8,1,2,5};
+    int size = 7;
+    int target = 5;
+    // cout<<linearSearch( arr,target,size);
+    reverseArr(arr,size);
+
+    for(int i=0; i<size;i++){
+        cout<< arr[i] <<" ";
+    }
+    cout << endl;
     return 0;
 }
